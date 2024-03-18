e44150c23fae73a16f993ac85916e3073d6f7c89
Matti Luukkainen <mluukkai@iki.fi>
2024-03-18 17:57:00 +0200

    tweak

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,21 @@
 // @ts-nocheck
 // eslint-disable-next-line no-undef
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('')
+  })
 
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('can follow a link', async ({ page }) => {
+    await page.getByText('ivysaur').click()
+
+    await expect(page.getByText('Overgrow')).toBeVisible()
+    await expect(page.getByText('Chlorophyll')).toBeVisible()
   })
 })
